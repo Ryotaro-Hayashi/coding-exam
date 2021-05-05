4a6a0adc6abf3f0af57a8bfbaa5c7fea64018a11
林　遼太朗 <hys.rtr.forit@gmail.com>
2021-05-05 19:10:02 +0900

    二分探索木は再起呼び出しで計算する. rootでの計算をデフォルトにする意識.

---
+++ leetcode/range_sum_of_BST.go
@@ -0,0 +1,32 @@
+package leetcode
+
+/**
+* Definition for a binary tree node.
+* type TreeNode struct {
+*     Val int
+*     Left *TreeNode
+*     Right *TreeNode
+* }
+*/
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Val < low { // 最低値より小さいとき
+		return rangeSumBST(root.Right, low, high) // 右側ノードで再度計算
+	}
+	if high < root.Val { // 最高値より大きいとき
+		return rangeSumBST(root.Left, low, high) // 左側ノードで再度計算
+	}
+
+	// rootと左側ノードの該当範囲と右側ノードの該当範囲を足して返す
+	return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+}
